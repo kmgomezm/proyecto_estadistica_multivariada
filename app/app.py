e75110d51ef324de0dfcfe9d1e162d92726f6438
kmgomezm <karengomez99@outlook.com>
2026-04-16 18:02:57 -0500

    Arreglos app

--- app/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="House Price App", layout="wide")
-
-st.title("🏠 House Price Prediction App")
-st.info(
-    "Usa el menú lateral de Streamlit para entrar a: "
-    "Test individual, Test por CSV, EDA, SHAP y Comparación de modelos."
-)
