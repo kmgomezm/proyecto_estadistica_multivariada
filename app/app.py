342995351e1850f7a643c76dd753b8bd0a177336
kmgomezm <karengomez99@outlook.com>
2026-04-16 17:27:50 -0500

    app

--- app/app.py
+++ app/app.py
@@ -4,6 +4,6 @@ st.set_page_config(page_title="House Price App", layout="wide")
 
 st.title("🏠 House Price Prediction App")
 st.info(
-    "Esta app usa navegación multipágina. Usa el menú lateral de Streamlit para entrar a: "
+    "Usa el menú lateral de Streamlit para entrar a: "
     "Test individual, Test por CSV, EDA, SHAP y Comparación de modelos."
 )
